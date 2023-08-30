8017928fe47ca550d1693f2712416a8fbd93f3cc
Ana <juhcristina1101@gmail.com>
2023-08-30 13:59:45 -0300

    arquivos novos

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-im
-import quemSomos from "./components/pages/QuemSomos"
 import Home from "./components/pages/Home"
+import QuemSomos from "./components/pages/QuemSomos"
+import Page404 from "./components/pages/Page404"
+import {Routes, Route} from "react-router-dom"
 import "./App.css"
-import { Route } from "react-router-dom"
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<home />} />
-      <Route path="quem-somos" element={<quemSomos />} />
-      <Route path="/" element={<Page404 />} />
-
-
-
+      <Route path="/" element={<Home />} />
+      <Route path="/quem-somos" element={<QuemSomos />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   )
 }
