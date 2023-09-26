dea6a37e8b831c0ce276179423b885c7460199bd
Ana <juhcristina1101@gmail.com>
2023-09-26 17:45:17 -0300

    arquivos novos

--- src/components/pages/Contato.jsx
+++ src/components/pages/Contato.jsx
@@ -1,19 +1,22 @@
-import Sidebar from "../layout/Sidebar"
-import Header from "../layout/Header"
 import Footer from "../layout/Footer"
-
+import Header from "../layout/Header"
+import Sidebar from "../layout/Sidebar"
+import Content from "../layout/Content"
 
 const Contato = () => {
-    return (
-      <>
-      <Header />
-      <div id="main">
-          <Sidebar />
-          <h1>Contato📱</h1>
-      </div>
-      <Footer />
-      </>
-    )
+  return (
+    <>
+        <Header />
+        <div id="main">
+            <Sidebar />
+            <Content>
+                <h1>Contato</h1>
+                <p>Entre em contato conosco através do formulário abaixo:</p>
+            </Content>
+        </div>
+        <Footer />
+    </>
+  )
 }
 
 export default Contato
--- src/components/pages/QuemSomos.jsx
+++ src/components/pages/QuemSomos.jsx
@@ -2,15 +2,42 @@ import Header from "../layout/Header"
 import Sidebar from "../layout/Sidebar"
 import Content from "../layout/Content"
 import Footer from "../layout/Footer"
+import CardUser from "../ui/CardUser"
+import { useEffect, useState } from "react"
+
 
 const QuemSomos = () => {
+
+    const [users, setUsers] = useState([])
+     
+
+    useEffect(()=>{
+      
+      const getUsers = async () => {
+        const response = await fetch('http://localhost:3300/user/list')
+        const data = await response.json()
+        console.log(data.success)
+        console.log(data.users)
+        setUsers(data.users)
+      }
+
+      getUsers()
+      
+    }, [])
+
   return (
     <>
         <Header />
         <div id="main">
             <Sidebar />
-            <h1>Quem Somos🧑‍🤝‍🧑</h1>
-            <Content />
+            <Content>
+              <h1>Quem Somos</h1>
+              {
+                users.length > 0 ? users.map((user) => {
+                  return <CardUser key={user.id} user={user} />
+                }): <p>Carregando...</p>
+              }
+            </Content>
         </div>
         <Footer />
     </>
