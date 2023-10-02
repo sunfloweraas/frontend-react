5db2439a6d92e85f6be787fd4c8a7bbbd95a4667
Ana <juhcristina1101@gmail.com>
2023-10-02 17:31:38 -0300

    novos

--- src/components/pages/QuemSomos.jsx
+++ src/components/pages/QuemSomos.jsx
@@ -5,17 +5,20 @@ import Footer from "../layout/Footer"
 import CardUser from "../ui/CardUser"
 import { useEffect, useState } from "react"
 import Button from "react-bootstrap/Button"
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
 
 
 const QuemSomos = () => {
 
     const [users, setUsers] = useState([])
+    const [showModal, setShowModal] = useState(false)
      
 
     useEffect(()=>{
       
       const getUsers = async () => {
-        const response = await fetch('http://localhost:3300/user/list')
+        const response = await fetch('http://localhost:3000/user/list')
         const data = await response.json()
         console.log(data.success)
         console.log(data.users)
@@ -26,6 +29,32 @@ const QuemSomos = () => {
       
     }, [])
 
+    const handleSubmit = async (event) => {
+      event.preventDefault()
+      const newUser = {
+        name: event.target.name.value,
+        email: event.target.email.value,
+        pass: event.target.pass.value,
+        photo: event.target.photo.value
+      }
+      
+      const response = await fetch('http://localhost:3000/user',{
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+      })
+
+      if(response.ok){
+        const data = await response.json()
+        alert(data.success)
+        setShowModal(false)
+        setUsers([...users, data.user])
+        
+      }
+    }  
+
   return (
     <>
         <Header />
@@ -33,7 +62,46 @@ const QuemSomos = () => {
             <Sidebar />
             <Content>
               <h1>Quem Somos</h1>
-              <Button as="button" variant="primary">Cadastrar Usuário</Button>
+              <Button as="button" variant="primary" onClick={() => setShowModal(true)}>Cadastrar Usuário</Button>
+
+              <Modal
+                show={showModal}
+                onHide={() => setShowModal(false)}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id="contained-modal-title-vcenter">
+                    Cadastrar Usuário
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Nome</Form.Label>
+                      <Form.Control type="text" name="name"/>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Email</Form.Label>
+                      <Form.Control type="email" name="email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>Senha</Form.Label>
+                      <Form.Control type="password" name="pass" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                      <Form.Label>URL Foto</Form.Label>
+                      <Form.Control type="text" name="photo"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit">Cadastrar</Button>
+                  </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button onClick={() => setShowModal(false)}>Close</Button>
+                </Modal.Footer>
+              </Modal>
+
               {
                 users.length > 0 ? users.map((user) => {
                   return <CardUser key={user.id} user={user} />
